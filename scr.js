e556674bb5904a60143b4006edbd9307a72b0330
sami1sa <86625201+sami1sa@users.noreply.github.com>
2021-06-28 22:21:25 +0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,40 @@
+var firstsliderrang = document.getElementById("firstrange");
+var firstoutput = document.getElementById("firstdemo");
+firstoutput.innerHTML = firstsliderrang.value;
+firstsliderrang.oninput = function() {
+    firstoutput.innerHTML = this.value;}
+
+
+    var secondsliderrang = document.getElementById("secondrange");
+    var secondoutput = document.getElementById("seconddemo");
+    secondoutput.innerHTML = secondsliderrang.value;
+    secondsliderrang.oninput = function() {
+    secondoutput.innerHTML = this.value;}
+
+
+ var thirdsliderrang = document.getElementById("thirdrange");
+ var thirddoutput = document.getElementById("thirddemo");
+ secondoutput.innerHTML = thirdsliderrang.value;
+ thirdsliderrang.oninput = function() {
+thirddoutput.innerHTML = this.value;}
+
+
+var forthsliderrang = document.getElementById("forthrange");
+var forthdoutput = document.getElementById("forthdemo");
+forthdoutput.innerHTML = forthsliderrang.value;
+forthsliderrang.oninput = function() {
+forthdoutput.innerHTML = this.value;}
+
+
+var fifthsliderrang = document.getElementById("fifthrange");
+var fifthdoutput = document.getElementById("fifthdemo");
+fifthdoutput.innerHTML = fifthsliderrang.value;
+fifthsliderrang.oninput = function() {
+fifthdoutput.innerHTML = this.value;}
+
+
+var sixthsliderrang = document.getElementById("sixthrange");
+var sixthdoutput = document.getElementById("sixthdemo");
+sixthdoutput.innerHTML = sixthsliderrang.value;
+sixthsliderrang.oninput = function() {
+sixthdoutput.innerHTML = this.value;}
